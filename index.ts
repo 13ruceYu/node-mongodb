5a1883a0eb81e3e6fc945bf4bfd8a0d655591947
13ruceYu <bruceyuhongbo@gmail.com>
2023-01-07 17:46:10 +0800

    feat: learn mongodb

---
+++ index.ts
@@ -0,0 +1,63 @@
+import { MongoClient } from "mongodb"
+
+const url = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(url)
+
+const records = [
+  {
+    name: 'Bobo',
+    age: 25
+  },
+  {
+    name: 'Bruce',
+    age: 26
+  }
+]
+
+async function run() {
+  try {
+    await client.connect()
+    const db = client.db('hello')
+    const res = await db.command({ping: 1})
+    console.log('connected', res)
+    const userCollection = db.collection('user')
+    // 数据插入
+    const result = await userCollection.insertOne({name: 'Jim', age: '16'})
+    // console.log(result)
+    // const resMany = await userCollection.insertMany(records)
+    // console.log(resMany)
+    // 数据查询
+    // const result = await userCollection.findOne({ name: 'Bobo' })
+    // console.log('one find:', result)
+    // 1 query with cursor
+    // const cursor = userCollection.find()
+    // await cursor.forEach(doc => console.log(doc))
+    // 2 query with toArray()
+    // const results = await userCollection.find().toArray()
+    // console.log(results)
+    // 3 query with comparison: $eq, $gt, $gte, $in, $lt, $lte, $ne, $nin
+    // const results = await userCollection.find({age: {$gt: 20}}).toArray()
+    // console.log(results)
+    // 4 query with logical: $and, $or, $not, $nor
+    // const results = await userCollection.find({
+    //   $or: [
+    //     { name: { $eq: 'Bruce' } },
+    //     { name: {$eq: 'AD'}}
+    //   ]
+    // }).toArray()
+    // console.log(results)
+    // 5 query with element: $exists, $type
+    // const results = await userCollection.find({hobby: {$exists: true}}).toArray()
+    // console.log(results)
+    const results = await userCollection.find({age: {$type: 'string'}}).toArray()
+    console.log(results)
+    
+
+  } catch (e) {
+    console.error(e)
+  } finally {
+    await client.close()
+  }
+}
+
+run()
